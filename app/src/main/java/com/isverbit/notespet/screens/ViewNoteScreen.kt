f9d3b57cbdd312e5e6e938676de0afbfc5464cd1
Igor Verbitskiy <igor.verbitskiy@pprcard.ru>
2024-05-16 14:03:22 +0700

    Fix save, delete and cancel edit

--- app/src/main/java/com/isverbit/notespet/screens/ViewNoteScreen.kt
+++ app/src/main/java/com/isverbit/notespet/screens/ViewNoteScreen.kt
@@ -1,10 +1,17 @@
 package com.isverbit.notespet.screens
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
